f60bd71f5deec0f930bc0bfef379e6255db95c44
gabrielandreato <97205888+gabrielandreato@users.noreply.github.com>
2022-09-18 12:02:05 -0300

    implementação de requisição de dados via json server

--- src/components/Filtro/index.tsx
+++ src/components/Filtro/index.tsx
@@ -5,10 +5,10 @@ import { filtroDeEventos } from '../../state/atom';
 import style from './Filtro.module.scss';
 
 const Filtro: React.FC = () => {
-  
+
   const [data, setData] = useState('')
   const setFiltroDeEvento = useSetRecoilState<IFiltroDeEventos>(filtroDeEventos)
-  
+
   const submeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault()
     const filtro: IFiltroDeEventos = {}
@@ -17,16 +17,16 @@ const Filtro: React.FC = () => {
     } else {
       filtro.data = null
     }
-    setFiltroDeEvento(filtro);    
+    setFiltroDeEvento(filtro);
   }
 
   return (<form className={style.Filtro} onSubmit={submeterForm}>
     <h3 className={style.titulo}>Filtrar por data</h3>
-    <input 
-      type="date" 
+    <input
+      type="date"
       name="data"
       className={style.input}
-      onChange={evento => setData(evento.target.value)} 
+      onChange={evento => setData(evento.target.value)}
       placeholder="Por data"
       value={data} />
 
